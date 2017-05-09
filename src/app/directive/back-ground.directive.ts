fb034ab71af64f9aab508339e9bb932ce157d7df
Mamah Livinus <livinus134@gmail.com>
2017-05-09 14:57:16 +0100

    modified back-graound directive

--- src/app/directive/back-ground.directive.ts
+++ src/app/directive/back-ground.directive.ts
@@ -1,22 +1,17 @@
-import { Directive, ElementRef, Renderer } from '@angular/core';
 
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 @Directive({
   selector: '[backGround]'
 })
 export class BackGroundDirective {
-
-  constructor(private el: ElementRef, private render: Renderer) {  }
-  onFocus() {
-    /**
-     *the element you want to apply it in this case is input tag 
-     * the property you want to set for input tag
-     * value of property
-     */
-    //this.render.setElementStyle(this.el, 'width', '200' );
-   this.el.nativeElement.style.backgroundColor = 'black';
+  constructor(private el: ElementRef) { }
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight('yellow');
   }
-  onBlur() {
-    //this.render.setElementStyle( this.el, 'width', '120' );
-     this.el.nativeElement.style.backgroundColor = 'white';
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight(null);
+  }
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
   }
 }
